a0bfad2e81c33136ebe6fa1e1120de4652f16313
adeliasavitskaya <adeliasavitskaya@gmail.com>
2026-05-30 19:17:25 +0300

    Day 1: project structure, CMakeLists, sort_step.h

---
+++ src/core/sort_step.h
@@ -0,0 +1,28 @@
+#ifndef SORT_STEP_H
+#define SORT_STEP_H
+
+#include <vector>
+#include <string>
+
+enum class StepType {
+    COMPARE,
+    SWAP,
+    FIND_PIVOT,
+    MERGE,
+    SPLIT,
+    DONE
+};
+
+struct SortStep {
+    std::vector <int> array;
+    int first{-1};
+    int second{-1};
+
+    int left{-1};
+    int right{-1};
+    StepType type{StepType::DONE};
+
+    std::string description;
+};
+
+#endif
